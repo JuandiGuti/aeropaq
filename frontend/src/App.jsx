2785c9d99270748768f7ff706b99cac31dddc2cc
JuandiGuti <juandiegoguti2302@gmail.com>
2026-03-22 13:35:21 -0600

    New vite_react_tailwindcss proyect in blank

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-50">
+      <h1 className="text-2xl font-bold text-slate-800">
+        Aeropaq: Próximamente
+      </h1>
+    </div>
+  )
+}
+
+export default App
