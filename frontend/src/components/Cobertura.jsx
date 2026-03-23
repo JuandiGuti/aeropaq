a649a0f4a6a09e9dfa039c78853ec10b3f2ecbe7
JuandiGuti <juandiegoguti2302@gmail.com>
2026-03-22 23:59:45 -0600

    style: fix import case for Cobertura module CSS for deployment

--- frontend/src/components/Cobertura.jsx
+++ frontend/src/components/Cobertura.jsx
@@ -4,7 +4,7 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Country } from "country-state-city";
-import styles from "./Cobertura.module.css";
+import styles from "./cobertura.module.css";
 
 const codigosPermitidos = [
 	"GT",
